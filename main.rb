063e01c78a9f45015beff343ec041c4946ab3685
SamWhale <samwhale1999@gmail.com>
2018-01-07 16:25:05 +0000

    Updated with changes from twitchchat-recorder

--- main.rb
+++ main.rb
@@ -1,91 +1,96 @@
-require 'socket'
-require 'logger'
-require 'date'
+require "socket"
+require "inifile"
+require "date"
 require 'open-uri'
 require 'net/http'
-require 'inifile'
 
-class RainBot
+Thread.abort_on_exception=true
+
+class Bot
   #constants / set up variables
-  VERSION = '0.9'
 
-  botIni = IniFile.load('config.ini')
-  OAUTH = botIni['botinfo']['OAuth-key']
-  NICKNAME = botIni['botinfo']['bot-nickname']
+  botIni = IniFile.load("config.ini")
+  OAUTH = botIni["botinfo"]["OAuth-key"]
+  NICKNAME = botIni["botinfo"]["bot-nickname"]
 
-  def initialize(logger = nil)
-    @logger = logger || Logger.new(STDOUT)
+  def initialize
     @running = false
     @socket = nil
     @channelname = nil
     @messages = 0
-    @commands = 0
+    Struct.new("Message", :user, :message)
+  end
+
+  def getTime
+    "[#{DateTime.now.strftime("%H:%M")}]"
   end
 
   def splitTraffic (s)
     #:username!username@username.tmi.twitch.tv PRIVMSG #twitchchannel :Hello World
-    #<------------------Preamble-------------><command-><-Channel----><-msg------>
+    #<------------------Preamble-------------><command-><-Channel----><----msg---->
+
+    #Stripping
     s = s[1..s.length-1] #strip out first character
-    splitstring = s.split(' ')
-    out = []
-    messagestart =  s.index(':')
+    splitstring = s.split(" ")
+    out = Struct::Message.new
+    messagestart =  s.index(":")
 
     #preamble contains the name 3 times and 17 other characters
     namelength = (splitstring[0].length - 17) / 3
-    out[0] = splitstring[0][0..namelength] #strip the name out of the preamble
-    out[1] = s[(messagestart+1)..s.length-1]
+    out.user = splitstring[0][0..namelength] #strip the name out of the preamble
+    out.message = s[(messagestart+1)..s.length-1]
     return out
   end
 
   #get channel and open socket
   def connect()
-    @logger.info "ShipBot v#{VERSION} started"
-    @logger.info('What twitch channel do you want to connect to?')
+    puts "#{getTime}: Ship Bot started"
+    puts "#{getTime}: What twitch channel do you want to connect to?"
     @channelname = gets.chomp.downcase
 
-    @logger.info('Preparing to connect...')
-    @socket = TCPSocket.new('irc.chat.twitch.tv', 6667)
-    @logger.info('Connected successfully')
+    puts "#{getTime}: Preparing to connect..."
+    @socket = TCPSocket.new("irc.chat.twitch.tv", 6667)
+    puts "#{getTime}: Connected successfully"
 
-    @logger.info('Authenticating...')
+    puts "#{getTime}: Authenticating..."
     @socket.puts("PASS #{OAUTH}")
     @socket.puts("NICK #{NICKNAME}")
-    @logger.info('Successfully authenticated')
+    puts "#{getTime}: Successfully authenticated"
 
-    @logger.info("Joining #{@channelname}...")
-    @socket.puts('JOIN #' + @channelname)
-    @logger.info("Joined #{@channelname}")
+    puts "#{getTime}: Joining #{@channelname}..."
+    @socket.puts("JOIN #" + @channelname)
+    puts "#{getTime}: Joined #{@channelname}"
 
-    @logger.info('Requesting permissions...')
-    @socket.puts('CAP REQ :twitch.tv/membership')
-    @logger.info('Permissions granted')
+    puts "#{getTime}: Requesting permissions..."
+    @socket.puts("CAP REQ :twitch.tv/membership")
+    puts "#{getTime}: Permissions granted"
     puts "" #newline
   end
 
   def run()
     running = true
 
-    #Main loop thread
-    Thread.start do
+    #Get messages and display them
+    messageReader = Thread.new do
       while (running) do
-        line = ''
+        line = ""
         traffic = IO.select([@socket]) #get socket traffic
         traffic[0].each do |s| #for each line
-          line = s.gets.chomp #get the line
+          line = s.gets #get the line
+          if line != nil
+            line = line.chomp
+          end
 
-          #Removes join and unesscesscary lines from the console
+          #Reads only chat messages
           if line =~ /:.+!.+@.+\.tmi\.twitch\.tv PRIVMSG #.+ :.+/
             #outputs chat wsith stripping
             line = splitTraffic(line)
-            print "#{line[0]}"
-            print " "*(25 - line[0].length) #25 character padding
-            puts ": #{line[1]}"
-            @messages = @messages+1
-          else
-            puts line
+            puts "#{getTime} #{line.user}#{" "*(25 - line.user.length)}:#{line[1]}"
+            @messages += 1
+          elsif line == "PING :tmi.twitch.tv"
+            @socket.puts("PONG :tmi.twitch.tv")
+            puts "\n#{getTime} INFO - Ping recieved. Pong sent.\n"
           end
-
-          #<--------Commands/Triggers-------->
 
           if line[1].downcase.include? "good bot"
             @socket.puts("PRIVMSG ##{@channelname} :Awh thanks <3")
@@ -94,12 +99,10 @@ class RainBot
           if line[1].downcase =~ /!time.*/
             time = DateTime.now.strftime("%d/%m/%Y %H:%M")
             @socket.puts("PRIVMSG ##{@channelname} :The time in GMT is #{time}")
-            @commands = @commands+1
           end
 
           if line[1].downcase =~ /!github.*/
             @socket.puts("PRIVMSG ##{@channelname} :My github repository can be found at https://github.com/SamWhale/ShipBot")
-            @commands = @commands+1
           end
 
           if line[1].downcase =~ /!commands.*/
@@ -107,8 +110,7 @@ class RainBot
           end
 
           if line[1].downcase =~ /!uptime.*/
-            uri = URI("https://decapi.me/twitch/uptime?channel=#{@channelname}")
-            text = Net::HTTP.get(uri)
+            text = Net::HTTP.get('decapi.me', "/twitch/uptime?channel=#{@channelname}")
 
             if text == "#{@channelname} is offline"
               @socket.puts("PRIVMSG ##{@channelname} :#{text}")
@@ -116,32 +118,35 @@ class RainBot
               @socket.puts("PRIVMSG ##{@channelname} :#{@channelname} has been live for #{text}")
             end
           end
+
         end
+
       end
     end
 
-    #console loop
-    while (running) do
-      command = gets.chomp
+    #Get user input
+    userinputCheck = Thread.new do
+      $command = gets
+    end
 
-      if command == 'disconnect'
+    #quit loop
+    while running do
+      if $command =~ /disconnect|dc/
         running = false
-      elsif command =~ /send .+/
-        msg = command[5..(command.length-1)]
-        @socket.puts("PRIVMSG ##{@channelname} :#{msg}")
-      else
-        @socket.puts(command)
       end
+      sleep(0.25)
     end
 
-    @logger.info 'RainprowBot closing...'
-    @logger.info "#{@messages} messages read"
-    @logger.info "#{@commands} commands read"
-    @logger.info "RainprowBot closed."
+    userinputCheck.kill
+    messageReader.kill
+
+    puts "#{getTime} Ship Bot closing..."
+    puts "#{getTime} #{@messages} messages read"
+    puts "#{getTime} Ship Bot closed."
   end
 end
 
-client = RainBot.new
+client = Bot.new
 quit = false
 
 while quit == false
@@ -150,7 +155,7 @@ while quit == false
 
   correctanswer = false
   while !correctanswer do
-    puts 'Connect to another channel? (Y/N)'
+    puts "Connect to another channel? (Y/N)"
     inp = gets
     if inp =~ /(n|N).*/
       correctanswer = true
@@ -158,7 +163,7 @@ while quit == false
     elsif inp =~ /(y|Y).*/
       correctanswer = true
     else
-      puts 'Please answer (y/n)'
+      puts "Please answer (y/n)"
     end
   end
 end
